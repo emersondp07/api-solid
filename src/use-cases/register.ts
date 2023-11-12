cb37245b080b7f68f166a47f7f3faec199e8bd26
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-11-11 21:52:34 -0300

    create first unit test in the module use-case register

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,3 +1,4 @@
+import { User } from "@prisma/client";
 import { hash } from "bcryptjs";
 import { UsersRepository } from "../repositories/users-repository";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
@@ -12,10 +13,18 @@ interface RegisterUseCaseRequest {
 
 // D - Dependency Inversion Principle
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -24,10 +33,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return { user };
   }
 }
