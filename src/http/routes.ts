f9ef47ba0f7e58d803697f9d8784808257714c96
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-11-20 11:11:09 -0300

    created e2e on the controller register, authenticate and profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -6,7 +6,7 @@ import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
-  app.post("/session", authenticate);
+  app.post("/sessions", authenticate);
 
   /** Authenticated */
   app.get("/me", { onRequest: [verifyJWT] }, profile);
